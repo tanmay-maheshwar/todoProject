d0eb28739cb6945f2fb2c8bcd9ea0844dfd02844
Tanmay <chiragbabrala123@gmail.com>
2024-11-21 01:07:14 +0530

    Created DesktopApp and WebApp

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
 
 def add_todo():
@@ -9,18 +10,21 @@ def add_todo():
     functions.write_todos(todos)
 
 
-
-
-
 st.title("My Todo App")
 st.subheader("This is a app for recording todos.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
-st.text_input(label="",placeholder="Add new todo",on_change=add_todo,key="new_todo")
+
+
+st.text_input(label ='',placeholder="Add new todo",on_change=add_todo,key="new_todo")
 
 
 
-st.session_state
